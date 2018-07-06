3a9c9c03d427df861af1160675fcf3c1095cb4bd
JanHuang <bboyjanhuang@gmail.com>
2018-07-06 11:20:20 +0800

    init

---
+++ src/Proxy/ProxyServer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author    jan huang <bboyjanhuang@gmail.com>
+ * @copyright 2018
+ *
+ * @see      https://www.github.com/fastdlabs
+ * @see      http://www.fastdlabs.com/
+ */
+
+namespace FastD\Sentinel\Proxy;
+
+
+use FastD\Servitization\Server\HTTPServer;
+
+class ProxyServer extends HTTPServer
+{
+
+}
